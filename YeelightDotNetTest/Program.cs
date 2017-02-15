13e88865c72c3a3d2c0bd52b25648bde670d3640
Jackson Ming Hu <huming2207@gmail.com>
2017-02-15 14:22:25 +0800

    Fix some null reference exceptions

--- YeelightDotNetTest/Program.cs
+++ YeelightDotNetTest/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using YeelightHelper;
 
-namespace YeelightSsdpTest
+namespace YeelightDotNetTest
 {
     class Program
     {
--- YeelightDotNetTest/Properties/AssemblyInfo.cs
+++ YeelightDotNetTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("YeelightSsdpTest")]
+[assembly: AssemblyTitle("YeelightDotNetTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("YeelightSsdpTest")]
+[assembly: AssemblyProduct("YeelightDotNetTest")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
